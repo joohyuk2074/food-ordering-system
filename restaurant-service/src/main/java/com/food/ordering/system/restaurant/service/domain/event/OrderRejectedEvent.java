256834526a0717ae439e2ce5504d964e0095fa18
weekbelt4207 <75966556+weekbelt4207@users.noreply.github.com>
2024-08-17 21:27:22 +0900

    56. Domain core module: Adding Exception, Domain events and Domain Service

---
+++ restaurant-service/src/main/java/com/food/ordering/system/restaurant/service/domain/event/OrderRejectedEvent.java
@@ -0,0 +1,28 @@
+package com.food.ordering.system.restaurant.service.domain.event;
+
+import com.food.ordering.system.application.handler.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.application.handler.domain.valueobject.RestaurantId;
+import com.food.ordering.system.restaurant.service.domain.entity.OrderApproval;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class OrderRejectedEvent extends OrderApprovalEvent {
+
+    private final DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher;
+
+    public OrderRejectedEvent(
+        OrderApproval orderApproval,
+        RestaurantId restaurantId,
+        List<String> failureMessages,
+        ZonedDateTime createdAt,
+        DomainEventPublisher<OrderRejectedEvent> orderApprovalEventDomainEventPublisher
+    ) {
+        super(orderApproval, restaurantId, failureMessages, createdAt);
+        this.orderRejectedEventDomainEventPublisher = orderApprovalEventDomainEventPublisher;
+    }
+
+    @Override
+    public void fire() {
+        orderRejectedEventDomainEventPublisher.publish(this);
+    }
+}
