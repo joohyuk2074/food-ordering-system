eb9277515ab0f206a80337b5c7702aaa3b690a27
weekbelt4207 <75966556+weekbelt4207@users.noreply.github.com>
2024-08-18 14:01:47 +0900

    63. Implementing Order Payment Saga

--- order-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
+++ order-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.food.ordering.system.order.service.domain.ports.output.repository;
 
 
+import com.food.ordering.system.domain.valueobject.OrderId;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import java.util.Optional;
@@ -10,4 +11,6 @@ public interface OrderRepository {
     Order save(Order order);
 
     Optional<Order> findByTrackingId(TrackingId trackingId);
+
+    Optional<Order> findById(OrderId orderId);
 }
